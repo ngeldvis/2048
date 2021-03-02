3622f96394b2a8dd9d47c3b479027366dd1538d3
Nigel Davis <ngeldvis@gmail.com>
2021-03-02 17:02:49 -0500

    small animation changes

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Recreation of the popular 2048 game created by Gabriele Cirulli (found [here](https://play2048.co/ "Original 2048 Game")) made in python using pygame
 
+# Controls
+
+use the arrow keys to move the board. press C to restart the game. press X to close the game
+
 # Requirements
 
 - Python 3
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from constants import *
 ARROW_KEYS = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
 FPS = 60
 
+
 # set the initial conditions of the game window
 def set_window() -> pygame.Surface:
     window = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,6 +20,7 @@ def set_window() -> pygame.Surface:
     pygame.display.set_caption('2048')
     window.fill(WHITE)
     return window
+
 
 # execute move required based on user input
 def make_move(board: Board, key: int, window: pygame.Surface) -> None:
@@ -43,10 +45,12 @@ def make_move(board: Board, key: int, window: pygame.Surface) -> None:
             board.add_block()
         board.draw(window)
 
+
 # clears and resets the board
 def reset_board(board: list, window: pygame.Surface) -> None:
     board.clear()
     board.draw(window)
+
 
 def draw_finish_screen(window: pygame.Surface, board: list) -> None:
     # faded background
@@ -66,18 +70,23 @@ def draw_finish_screen(window: pygame.Surface, board: list) -> None:
     text_placement = (text_x, text_y)
     window.blit(text, text_placement)
 
+
 def over(window: pygame.Surface, board: list) -> None:
     draw_finish_screen(window, board)
+
 
 def draw_button(window: pygame.Surface, mouse) -> None:
     # button box
     x_offset = RESTART_BUTTON_POS_W - RESTART_BUTTON_WIDTH // 2
     y_offset = RESTART_BUTTON_POS_H - RESTART_BUTTON_HEIGHT // 2
-    button = pygame.Rect(x_offset, y_offset, RESTART_BUTTON_WIDTH, RESTART_BUTTON_HEIGHT)
+    button = pygame.Rect(x_offset, y_offset,
+                         RESTART_BUTTON_WIDTH, RESTART_BUTTON_HEIGHT)
     if mouse_on_button(mouse):
-        pygame.draw.rect(window, COLORS['border'], button, border_radius=BLOCK_RADIUS)
+        pygame.draw.rect(window, COLORS['border'],
+                         button, border_radius=BLOCK_RADIUS)
     else:
-        pygame.draw.rect(window, COLORS['dark_text'], button, border_radius=BLOCK_RADIUS)
+        pygame.draw.rect(
+            window, COLORS['dark_text'], button, border_radius=BLOCK_RADIUS)
     # 'try again' text
     font = pygame.font.Font(FONT, 30)
     text = font.render("Try again", True, WHITE)
@@ -86,8 +95,10 @@ def draw_button(window: pygame.Surface, mouse) -> None:
     text_placement = (text_x, text_y)
     window.blit(text, text_placement)
 
+
 def mouse_on_button(mouse):
     return RESTART_BUTTON_POS_W - RESTART_BUTTON_WIDTH // 2 <= mouse[0] <= RESTART_BUTTON_POS_W + RESTART_BUTTON_WIDTH // 2 and RESTART_BUTTON_POS_H - RESTART_BUTTON_HEIGHT // 2 <= mouse[1] <= RESTART_BUTTON_POS_H + RESTART_BUTTON_HEIGHT // 2
+
 
 # main function with event loop
 def main() -> None:
@@ -127,17 +138,18 @@ def main() -> None:
                     running = False
 
             if game_over == True:
-                if event.type == pygame.MOUSEBUTTONDOWN: 
+                if event.type == pygame.MOUSEBUTTONDOWN:
                     # 'new game' button pressed
                     if mouse_on_button(mouse):
                         reset_board(board, window)
                         game_over = False
-        
+
         mouse = pygame.mouse.get_pos()
 
         if game_over == True:
             draw_button(window, mouse)
 
         pygame.display.update()
+
 
 main()
